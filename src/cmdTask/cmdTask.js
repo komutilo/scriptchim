a2522d2aa85d284b9e304805a3fb4f85a5ebb079
Bruno Araujo <bruno@komutilo.com>
2024-02-08 04:21:07 -0300

    feat: multiple small fixes to publish (#1)

--- src/cmdTask/cmdTask.js
+++ src/cmdTask/cmdTask.js
@@ -2,14 +2,28 @@ const chalk = require('chalk');
 const { execCmd } = require('../execCmd');
 
 /**
+ * Execute an asynchronous cli command as a process.
+ *
+ * @name cmdTask
+ * @function
+ * @param {string[] | string} args An array or whitespace separated string with cli args.
+ * @returns {undefined}
+ * @throws {InvalidTypeError} if type of args param is invalid.
+ */
+/**
  * Execute an asynchronous cli command as a process, if succeed run a callback.
  *
- * @param {function | undefined} next A callback function.
+ * @name cmdTask^2
+ * @function
+ * @param {function | string[] | string} next A callback function.
  * @param {string[] | string} args An array or whitespace separated string with cli args.
  * @returns {undefined}
  * @throws {InvalidTypeError} if type of args param is invalid.
  */
 async function cmdTask(next, args) {
+  if (next && typeof next !== 'function' && (typeof next === 'string' || typeof next === 'object')) {
+    args = next;
+  }
   const ps = await execCmd(args);
   if (ps.code !== 0) process.exit(ps.code);
   if (next && typeof next === 'function') {
